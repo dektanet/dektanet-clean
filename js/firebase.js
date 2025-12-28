0453ff089d2557afbcc0e3ede059e8e47b6e0cca
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-28 05:56:24 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "XXXX",
   authDomain: "XXXX.firebaseapp.com",
   projectId: "XXXX",
-  appId: "XXXX"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
