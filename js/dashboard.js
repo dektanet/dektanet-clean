603b2888112e5653a10eae8cfb3c30895aa96999
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-28 07:45:43 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,8 +2,11 @@ import { auth } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 onAuthStateChanged(auth, user => {
-  if (user) {
-    document.getElementById("userInfo").innerText =
-      `Email: ${user.email}\nUID: ${user.uid}`;
+  if (!user) {
+    location.href = "login.html";
+    return;
   }
+
+  document.getElementById("userInfo").innerText =
+    `Email: ${user.email}\nUID: ${user.uid}`;
 });
