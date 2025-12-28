48c4dcf68c5eef930866da4a514f18a6345a27d9
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-28 07:44:27 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -6,27 +6,36 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import {
-  doc, setDoc, serverTimestamp
+  doc,
+  setDoc,
+  serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+/* REGISTER */
 export async function register(email, password) {
   const cred = await createUserWithEmailAndPassword(auth, email, password);
+
   await setDoc(doc(db, "users", cred.user.uid), {
     uid: cred.user.uid,
-    email,
+    email: email,
     role: "user",
     createdAt: serverTimestamp()
   });
+
+  return cred;
 }
 
+/* LOGIN */
 export function login(email, password) {
   return signInWithEmailAndPassword(auth, email, password);
 }
 
+/* LOGOUT */
 export function logout() {
   return signOut(auth);
 }
 
+/* AUTH WATCHER */
 export function watchAuth(cb) {
   return onAuthStateChanged(auth, cb);
 }
