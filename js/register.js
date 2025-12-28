57193abaefb18e7d7a464a8b01993babd129b6a6
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-28 07:53:22 +0100

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,7 +1,4 @@
-import { auth } from "./firebase.js";
-import {
-  createUserWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { register } from "./auth.js";
 
 const email = document.getElementById("email");
 const password = document.getElementById("password");
@@ -9,15 +6,9 @@ const btn = document.getElementById("registerBtn");
 
 btn.addEventListener("click", async () => {
   try {
-    const userCred = await createUserWithEmailAndPassword(
-      auth,
-      email.value,
-      password.value
-    );
-
+    await register(email.value, password.value);
     alert("Account created ✅");
-    window.location.href = "index.html";
-
+    window.location.href = "dashboard.html";
   } catch (err) {
     alert(err.message);
   }
