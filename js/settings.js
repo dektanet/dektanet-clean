008a17d113e6b234f87453556ab5469a5b1fafff
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-28 03:09:57 +0100

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,8 @@
 // Global settings loader
 
+import { applyTheme } from "./theme.js";
+import { setLanguage, detectLanguage } from "./i18n.js";
+
 document.addEventListener("DOMContentLoaded", () => {
   // Theme
   applyTheme(localStorage.getItem("theme") || "dark");
