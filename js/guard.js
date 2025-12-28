9af0f09ae18598eb0df87bfc91a7621aed8973db
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-28 07:17:49 +0100

    Update guard.js

--- js/guard.js
+++ js/guard.js
@@ -1,10 +1,10 @@
 import { auth } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-export function protectPage() {
-  onAuthStateChanged(auth, user => {
-    if (!user) {
-      window.location.href = "login.html";
-    }
-  });
-}
+onAuthStateChanged(auth, user => {
+  if (!user) {
+    // ❌ موش مسجّل → نرجّعو للـ login
+    window.location.href = "login.html";
+  }
+  // ✅ مسجّل → يخليه يكمل
+});
