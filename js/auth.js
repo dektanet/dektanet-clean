da9533d38cecfe1f6097bf7a18012fcc17314b13
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-29 21:43:47 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,21 +1,8 @@
+// js/auth.js
 import { auth } from "./firebase.js";
-import {
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { signInWithEmailAndPassword }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 export async function login(email, password) {
-  const userCredential =
-    await signInWithEmailAndPassword(auth, email, password);
-
-  return userCredential.user;
-}
-
-export function logout() {
-  return signOut(auth);
-}
-
-export function onUserChanged(callback) {
-  onAuthStateChanged(auth, callback);
+  await signInWithEmailAndPassword(auth, email, password);
 }
