419b9c95d4aa27e6e50238d4efb7373e5a676766
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-29 14:53:40 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,30 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-onAuthStateChanged(auth, user => {
+onAuthStateChanged(auth, async (user) => {
   if (!user) {
     location.href = "login.html";
     return;
   }
 
+  // جلب user data
+  const snap = await getDoc(doc(db, "users", user.uid));
+  const data = snap.data();
+
+  // عرض المعلومات
   document.getElementById("userInfo").innerText =
-    `Email: ${user.email}\nUID: ${user.uid}`;
+    `Email: ${user.email || data.phone}\nUID: ${user.uid}`;
+
+  document.getElementById("dektaBalance").innerText =
+    data.balances?.dekta ?? 0;
+
+  document.getElementById("boxBalance").innerText =
+    data.box?.bonus ?? 30;
+
+  document.getElementById("boxStatus").innerText =
+    data.box?.status ?? "INACTIVE";
 });
