8139687906318f7afe669b30e2e1f089d979e9c8
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-29 22:01:04 +0100

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,9 +1,10 @@
-// js/register.js
 import { auth, db } from "./firebase.js";
-import { createUserWithEmailAndPassword }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { doc, setDoc }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+  doc, setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 export async function register(email, password, phone) {
   const cred = await createUserWithEmailAndPassword(auth, email, password);
@@ -12,9 +13,7 @@ export async function register(email, password, phone) {
   await setDoc(doc(db, "users", user.uid), {
     email,
     phone,
-    dekta: 0,
-    dektaBox: 30,
-    boxStatus: "INACTIVE",
+    balance: 0,
     createdAt: Date.now()
   });
 
