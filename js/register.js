94585eceee18960ef69e9ec94660588ff613c7cc
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-29 22:52:39 +0100

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,18 +1,21 @@
 import { auth, db } from "./firebase.js";
 import { createUserWithEmailAndPassword }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+  from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { doc, setDoc }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+  from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 export async function register(email, password, phone) {
-  const cred = await createUserWithEmailAndPassword(auth, email, password);
+  const userCredential =
+    await createUserWithEmailAndPassword(auth, email, password);
 
-  await setDoc(doc(db, "users", cred.user.uid), {
+  const user = userCredential.user;
+
+  await setDoc(doc(db, "users", user.uid), {
     email,
-    phone,
+    phone: phone || "",
     balance: 0,
     createdAt: Date.now()
   });
 
-  return cred.user;
+  return user;
 }
