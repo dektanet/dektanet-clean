bc7886d96df15d8c8a09b5d573bd8f1d9cc175f5
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-29 21:26:51 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,3 @@
-// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
@@ -14,4 +13,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app); // 🔥 مهم
+export const db = getFirestore(app);
