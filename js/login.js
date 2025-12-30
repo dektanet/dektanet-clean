3bfe62222c9316b250b618d8757b0e609a218a59
dektator <77615025+dektanet@users.noreply.github.com>
2025-12-30 03:28:57 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,14 +1,24 @@
 import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { signInWithEmailAndPassword } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-document.getElementById("loginBtn").onclick = async () => {
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   try {
     await signInWithEmailAndPassword(auth, email, password);
-    window.location.href = "referral.html";
-  } catch (e) {
-    alert(e.message);
+
+    alert("Login success ✅");
+
+    // REDIRECT TO DASHBOARD
+    window.location.href = "dashboard.html";
+
+  } catch (error) {
+    alert(error.message);
   }
-};
+});
