84917b9840431c334e797c7cf97c455b2c7296ce
dektator <77615025+dektanet@users.noreply.github.com>
2026-01-02 15:06:42 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,8 +1,15 @@
 import { auth } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { onAuthStateChanged, signOut } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
   if (!user) {
     window.location.href = "login.html";
   }
 });
+
+document.getElementById("logoutBtn").onclick = () => {
+  signOut(auth).then(() => {
+    window.location.href = "login.html";
+  });
+};
