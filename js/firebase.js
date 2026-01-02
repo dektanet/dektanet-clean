f928387d06e2bfc050e63183cde1d1f8b95c6585
dektator <77615025+dektanet@users.noreply.github.com>
2026-01-02 17:46:15 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,17 @@
-// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// 🔴 بدّل القيم هاذم بقيم Firebase متاعك
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "PASTE_API_KEY_HERE",
+  authDomain: "dektanet.firebaseapp.com",
+  projectId: "dektanet",
+  storageBucket: "dektanet.appspot.com",
+  messagingSenderId: "PASTE_SENDER_ID",
+  appId: "PASTE_APP_ID"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
