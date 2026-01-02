875dcffe6dccf4cbfd8ce31ded35ef736ed4150b
dektator <77615025+dektanet@users.noreply.github.com>
2026-01-02 19:14:24 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,21 +1,15 @@
-import { auth, db } from "./firebase.js";
+import { auth } from "./firebase.js";
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
-onAuthStateChanged(auth, async (user) => {
+onAuthStateChanged(auth, user => {
   if (!user) {
-    window.location.href = "index.html";
-    return;
+    window.location.href = "login.html";
+  } else {
+    document.getElementById("userEmail").innerText = user.email;
   }
-
-  const snap = await getDoc(doc(db, "users", user.uid));
-  const data = snap.data();
-
-  document.getElementById("dekta").innerText = data.dekta;
-  document.getElementById("box").innerText = data.boxActive ? "ACTIVE" : "INACTIVE";
 });
 
 document.getElementById("logoutBtn").onclick = async () => {
   await signOut(auth);
-  window.location.href = "index.html";
+  window.location.href = "login.html";
 };
