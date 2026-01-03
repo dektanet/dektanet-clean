a1a8db43e602dd68b59c8440dd90617161a8cea0
dektator <77615025+dektanet@users.noreply.github.com>
2026-01-03 04:09:00 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,15 +1,37 @@
-import { auth } from "./firebase.js";
-import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { auth, db } from "./firebase.js";
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-onAuthStateChanged(auth, user => {
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
   if (!user) {
     window.location.href = "login.html";
-  } else {
-    document.getElementById("userEmail").innerText = user.email;
+    return;
+  }
+
+  document.getElementById("userEmail").innerText =
+    "Welcome: " + user.email;
+
+  const refLink = `${location.origin}/register.html?ref=${user.uid}`;
+  document.getElementById("refLink").value = refLink;
+
+  const userRef = doc(db, "users", user.uid);
+  const snap = await getDoc(userRef);
+
+  if (snap.exists()) {
+    const data = snap.data();
+    document.getElementById("dekta").innerText = data.dekta || 0;
+    document.getElementById("baby").innerText = data.baby || 0;
   }
 });
 
-document.getElementById("logoutBtn").onclick = async () => {
+window.logout = async function () {
   await signOut(auth);
   window.location.href = "login.html";
 };
